9a8cb64537e000fb840c145c5657ff93b19afd05
Savas ADAR <savas_adar@hotmail.com>
2017-08-14 15:28:21 +0300

    Lesson 1 resources uploaded.

---
+++ Lesson 1/RollaBall/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            gameObject.SetActive(false);
+            GameController.score++;
+            Invoke("setActiveEnemy", 2);
+        }
+
+        Debug.Log(collision.gameObject.name);
+    }
+
+    private void setActiveEnemy()
+    {
+        gameObject.SetActive(true);
+    }
+    
+}
---
+++ Lesson 1/RollaBall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        
+
+        Vector3 v3 = new Vector3(h, 0, v);
+
+        rb.AddForce(v3);
+    }
+}
